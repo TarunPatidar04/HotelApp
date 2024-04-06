deb9c4512eb2e73198775dd8790c4e6e13cefec1
TarunPatidar04 <tarun.coderfarm@gmail.com>
2024-04-06 14:48:36 +0530

    chnages shift mongodb to online server

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 //waiter===server
 const express = require("express");
+require("dotenv").config();
 const app = express();
 const db = require("./db");
 const bodyParser = require("body-parser");
 app.use(bodyParser.json()); //req.body
+const PORT=process.env.PORT || 3000
 
 app.get("/", (req, res) => {
   res.send(
@@ -19,6 +21,7 @@ const menuRoutes = require("./Routes/menuRoutes");
 app.use("/person", personRoutes);
 app.use("/menu", menuRoutes);
 
-app.listen(3000, () => {
-  console.log("server started");
+
+app.listen(PORT, () => {
+  console.log(`server started ${PORT}`);
 }); //room number
